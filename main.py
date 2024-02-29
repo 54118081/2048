a2d3a9cb842c95c23a090d1834ec765ec5b84ed0
54118081 <tangzilong1018@gmail.com>
2024-02-28 17:17:12 -0800

    create project 2048

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tk import ttk
+
+
+def game_start():
+    # start the game 2048
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    game_start()
+
+
